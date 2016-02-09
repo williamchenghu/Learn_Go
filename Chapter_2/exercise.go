7400e65cd1e86c0499685bc9d623936b0c5f87f9
williamchenghu <williamchenghu@users.noreply.github.com>
2016-02-09 16:36:56 +0200

    add Chapter 2 folder, repeat the example, do the exercise

---
+++ Chapter_2/exercise.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("32,132 x 42,452 =", 32132 * 42452)
+
+}
---
+++ Chapter_2/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	
+	//let's test some integers with calculation
+	fmt.Println("1 + 1 =", 1 + 1)
+
+	fmt.Println("2 - 2 =", 2 - 2)
+
+	fmt.Println("3 * 3 =", 3 * 3)
+
+	fmt.Println("4 / 4 =", 4 / 4)
+
+	//let's test some string
+	fmt.Println(len("Hello, World")) //len function calculates the length of this string "Hello, World", notice space counts as well
+
+	fmt.Println("Hello, World"[1]) //this will shows the 2nd character of sting "Hello, World", notice the 1st is 0, not 1, so 'e' should be shown instead of 'H', however, character is shown by a byte, which is also an integer, so 101 should show up when execute this program
+
+	fmt.Println("Hello, " + "World") //works almost like calculation on integers or floats. but those calculation doesn't make sense on strings, so instead, this will work as build up a sentence with multiple words.
+
+	//let's test some booleans
+	fmt.Println(true && true) //'&&' means AND
+
+	fmt.Println(true && false)
+
+	fmt.Println(true || true) //'||' means OR
+
+	fmt.Println(true || false)
+
+	fmt.Println(!true) //'!' means NOT
+}
